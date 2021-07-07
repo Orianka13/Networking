3dd60ae406900b464b1a073f65d87256b1cb09dd
Olesia <odobrogor@gmail.com>
2021-07-07 16:59:08 +0300

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Alexey Efimov on 25/07/2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func getRequest(_ sender: Any) {
+    }
+    
+    @IBAction func postRequest(_ sender: Any) {
+    }
+    
+}
+
