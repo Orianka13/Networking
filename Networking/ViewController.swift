f19242c0973bc4c535aa123652340ee87c618906
Olesia <odobrogor@gmail.com>
2021-07-07 17:16:40 +0300

    GET

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -11,6 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func getRequest(_ sender: Any) {
+        //проверим валидность нашего адреса
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")  else { return }
+        //создаем сессию
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            guard let response = response, let data = data else { return }
+            print(response)
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        } .resume()
     }
     
     @IBAction func postRequest(_ sender: Any) {
